d9a9b469b9115a31a6cf6f6ce9c4ed603f359d84
Rubab <ahmadrubab13@gmail.com>
2025-02-25 00:13:21 +0500

    Checker board pattern

--- checkerboard_pattern.py
+++ checkerboard_pattern.py
@@ -3,3 +3,12 @@
 #  * * * *
 # * * * * 
 #  * * * *
+
+rows = int(input("Enter the number of rows: "))
+
+for i in range(0, rows):
+  if not (i%2 == 0):
+    print(" ", end='')
+  for j in range(0, rows): 
+    print("* ", end='')
+  print()
