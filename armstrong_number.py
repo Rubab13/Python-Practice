79154d9388372d3aed65530eb5143155fac367d9
Rubab <ahmadrubab13@gmail.com>
2025-03-10 21:25:06 +0500

    Armstrong number

---
+++ armstrong_number.py
@@ -0,0 +1,7 @@
+# Check if a given number is an Armstrong number.
+# An Armstrong number is a number that equals the sum of its digits, each raised to the power of the number of digits.
+
+# number = int(input("Enter a number: "))
+
+# while (number >= 0):
+#   number 
